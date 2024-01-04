9fb54876ea3a08d8998e742be84451ed5e87240b
Alexis989898 <alexisvitor6@gmail.com>
2024-01-03 21:08:18 -0300

    Design update (unfinished)

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-const btnAdd = window.document.querySelector(".add-btn");
-
-btnAdd.addEventListener("click", openDialog);
+const btnAdd = document.querySelector(".add-btn");
+const dialogAdd = document.querySelector("#dialog-add");
+const closeBtn = document.querySelector(".close-btn");
 
 const myLibrary = [];
 
 function Book(name, author, pages, status) {
-  this.name = name;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
 }
 
 function addBookToLibrary() {
 }
+
+btnAdd.addEventListener("click", () => {
+    dialogAdd.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+    dialogAdd.close();
+})
